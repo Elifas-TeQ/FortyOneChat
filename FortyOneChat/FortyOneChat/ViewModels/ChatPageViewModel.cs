a3adfca9aec84d8ccd4ee92410416946148ea8fa
SOFTSERVE\vsov <revanmvs2@gmail.com>
2017-02-01 15:28:46 +0200

    Chat view

--- FortyOneChat/FortyOneChat/ViewModels/ChatPageViewModel.cs
+++ FortyOneChat/FortyOneChat/ViewModels/ChatPageViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+
+using System;
 using System.Collections.ObjectModel;
 using FortyOneChat.Core;
 using FortyOneChat.Core.Services;
 using Prism.Mvvm;
 using Prism.Navigation;
+using System.Windows.Input;
 
 namespace FortyOneChat.ViewModels
 {
@@ -12,6 +14,15 @@ namespace FortyOneChat.ViewModels
         private readonly IChatService _chatService;
 
         public ObservableCollection<Message> Messages { get; set; }
+
+        private string _newMessage;
+        public string NewMessage
+        {
+            get { return _newMessage; }
+            set { SetProperty(ref _newMessage, value); }
+        }
+
+        public ICommand SendMessage { get; set; }
         public ChatPageViewModel(IChatService chatService)
         {
             if (chatService == null) throw new ArgumentNullException(nameof(chatService));
