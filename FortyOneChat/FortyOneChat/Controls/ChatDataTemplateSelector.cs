6c49bd29116db620d7c550323d90ab925458665b
Ruslan Soromlya <rsorom@softserveinc.com>
2017-02-01 15:18:48 +0200

    Add fake services, add data template selector.

---
+++ FortyOneChat/FortyOneChat/Controls/ChatDataTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FortyOneChat.Controls.ViewCells;
+using FortyOneChat.Core;
+using FortyOneChat.Core.Services;
+using Xamarin.Forms;
+
+namespace FortyOneChat.Controls
+{
+    public class ChatDataTemplateSelector : Xamarin.Forms.DataTemplateSelector
+    {
+        private readonly DataTemplate _incomingMessageTemplate;
+        private readonly DataTemplate _outcommingMessageTemplate;
+        private readonly IApplicationContext _context;
+
+        public ChatDataTemplateSelector(IApplicationContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            _context = context;
+            _incomingMessageTemplate = new DataTemplate(typeof(IncomingViewCell));
+            _outcommingMessageTemplate = new DataTemplate(typeof(OutcomingViewCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var message = item as Message;
+            if (message == null) return null;
+            return message.AuthorId == _context.CurrentUser.Id ? _incomingMessageTemplate : _outcommingMessageTemplate;  
+        }
+    }
+}
--- FortyOneChat/FortyOneChat/ViewModels/ChatPageViewModel.cs
+++ FortyOneChat/FortyOneChat/ViewModels/ChatPageViewModel.cs
@@ -1,8 +1,35 @@
-﻿using Prism.Mvvm;
+﻿using System;
+using System.Collections.ObjectModel;
+using FortyOneChat.Core;
+using FortyOneChat.Core.Services;
+using Prism.Mvvm;
+using Prism.Navigation;
 
 namespace FortyOneChat.ViewModels
 {
-    public class ChatPageViewModel : BindableBase
+    public class ChatPageViewModel : BindableBase, INavigationAware
     {
+        private readonly IChatService _chatService;
+
+        public ObservableCollection<Message> Messages { get; set; }
+        public ChatPageViewModel(IChatService chatService)
+        {
+            if (chatService == null) throw new ArgumentNullException(nameof(chatService));
+
+            _chatService = chatService;
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+            
+        }
+
+        public async void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (Messages == null)
+            {
+                Messages = new ObservableCollection<Message>(await _chatService.GetMessages());
+            }
+        }
     }
 }
