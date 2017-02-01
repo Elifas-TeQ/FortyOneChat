2f956dd5a8b02547455477afa02ad76e3821d8db
Oleksandr Chaban <ochab@softserveinc.com>
2017-02-01 14:17:31 +0200

    added Models: Message, User

---
+++ FortyOneChat/FortyOneChat.Core/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FortyOneChat.Core
+{
+	public class Message
+	{
+		public int Id { get; set; }
+		public string Text { get; set; }
+		public int AuthorId { get; set; }
+		public DateTime DateTime { get; set; }
+	}
+}
