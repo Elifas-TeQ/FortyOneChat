9d88139a4e5389d3b295eaa0d5cce140d8c8364f
SOFTSERVE\atuz <atuz@softserveinc.com>
2017-02-01 15:59:24 +0200

    Add UserStatus, GetHistory, SendMessage and login methods and some requests

--- FortyOneChatServices/FortyOneChatServices/Controllers/ChatController.cs
+++ FortyOneChatServices/FortyOneChatServices/Controllers/ChatController.cs
@@ -1,41 +1,56 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
+using FortyOneChatServices.Models;
+using FortyOneChatServices.Models.Requests;
 
 namespace FortyOneChatServices.Controllers
 {
-    [System.Web.Http.RoutePrefix("api/Chat")]
+    [RoutePrefix("api/Chat")]
     public class ChatController : ApiController
     {
-        private const string LocalLoginProvider = "Local";
-        private ApplicationUserManager _userManager;
+        public static Dictionary<int, string> Users = new Dictionary<int, string>();
+        public static Dictionary<int, Message> Messages = new Dictionary<int, Message>();
 
-        public ChatController()
+        [HttpGet]
+        [Route("History")]
+        public IEnumerable<Message> GetHistory()
         {
+            return Messages.Values.Take<Message>(20); ;
         }
 
-        public ChatController(ApplicationUserManager userManager,
-            ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
+        [HttpPost]
+        [Route("Login")]
+        public bool Login(string userName)
         {
-            UserManager = userManager;
-            AccessTokenFormat = accessTokenFormat;
-        }
-
-        public ApplicationUserManager UserManager
-        {
-            get
+            bool res = Users.Values.Contains<string>(userName);
+            if (!res)
             {
-                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                Users.Add(Users.Keys.LastOrDefault<int>()+1,userName);
             }
-            private set
-            {
-                _userManager = value;
-            }
+
+            return !res;
+        }
+        
+        [HttpPost]
+        [Route("SendMessage")]
+        public bool SendMessage(Message message)
+        {
+            var index = Messages.Count + 1;
+            Messages.Add(index, message);
+           
+            return true;
         }
 
-        public ISecureDataFormat<AuthenticationTicket> AccessTokenFormat { get; private set; }
+        [HttpGet]
+        [Route("UserStatus")]
+        public bool  GetUserStatus()
+        {
+            return true;
+        }
         
     }
 }
