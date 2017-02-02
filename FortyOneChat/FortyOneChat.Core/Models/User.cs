2edbe66b916876fa28f26352e67aaf006e0dd52c
Ruslan Soromlya <rsorom@softserveinc.com>
2017-02-02 13:11:47 +0200

    Synchronize Xamarin.Forms package version, make LastTimeOnline field public.

--- FortyOneChat/FortyOneChat.Core/Models/User.cs
+++ FortyOneChat/FortyOneChat.Core/Models/User.cs
@@ -6,6 +6,6 @@ namespace FortyOneChat.Core.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		DateTime LastDateOnline { get; set; }
+		public DateTime LastDateOnline { get; set; }
 	}
 }
